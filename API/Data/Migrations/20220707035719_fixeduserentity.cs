869fd5c5e0df369eaab349c74d6727eee3f76140
MSandwidi <shoudou@outlook.com>
2022-07-07 00:38:32 -0500

    added DTOs

---
+++ API/Data/Migrations/20220707035719_fixeduserentity.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class fixeduserentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "passwordSalt",
+                table: "Users",
+                newName: "PasswordSalt");
+
+            migrationBuilder.RenameColumn(
+                name: "passwordHash",
+                table: "Users",
+                newName: "PasswordHash");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswordSalt",
+                table: "Users",
+                newName: "passwordSalt");
+
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Users",
+                newName: "passwordHash");
+        }
+    }
+}
