965cdffd2402a2da07b991e0ad9cde3a86e9d10d
MSandwidi <shoudou@outlook.com>
2022-07-10 15:36:21 -0500

    added api like controller

---
+++ API/Data/Migrations/20220710203133_addedlikesrepo.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class addedlikesrepo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Likes_Users_LikedUserId",
+                table: "Likes");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Likes_Users_LikedUserId",
+                table: "Likes",
+                column: "LikedUserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Likes_Users_LikedUserId",
+                table: "Likes");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Likes_Users_LikedUserId",
+                table: "Likes",
+                column: "LikedUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
